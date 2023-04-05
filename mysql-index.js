178cb210868e506d509196c67d2a54e787e858d1
nowjun <hyunjun1325@icloud.com>
2023-04-05 15:15:41 +0900

    added web pv pvc

--- mysql-index.js
+++ mysql-index.js
@@ -1,15 +1,15 @@
-const mysql = require('mysql');
+const mysql = require('mysql2');
 const connection = mysql.createConnection({
-        host    : 'mysql-service.default.svc.cluster.local',
+        host    : process.env.DB_URL,
         user    : process.env.MYSQL_USER,
         password: process.env.MYSQL_PASSWORD,
-        database: 'test_DB'
+        database: process.env.MYSQL_DATABASE
 });
 
 connection.connect();
 
 connection.query('select * from first', (error, rows, fields) => {
         if(error) throw error;
-        console.log(rows, fields);
+        console.log(rows);
 });
 connection.end();
